fe7fa2a37c4145f3fa3396e63025ec5227a769dc
eliezer o grande dev <eliezerassuncaocustodio@hotmail.com>
2025-11-25 20:05:14 -0300

    criado módulo para cores dos botões

---
+++ android/barradenavegacao/src/main/java/com/megalodonte/barradenavegacao/RegisterPackage.kt
@@ -0,0 +1,17 @@
+package com.megalodonte.barradenavegacao
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class BarraDeNavegacaoPackage : ReactPackage {
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(BarraDeNavegacaoModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
